1a3c1e2216c41c3ca665924d4b2590d1697d8d5b
Manoela Urias <maanoelaurias@gmail.com>
2024-10-30 21:44:19 -0300

    Criação do componente

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="container-home">
-
+        <header>
+            <ComponenteHeader/>
+        </header>
   
     </div>
 </template>
 
 <script setup lang="ts">
+import ComponenteHeader from '@/components/shared/ComponenteHeader/ComponenteHeader.vue';
 
 </script>
 
