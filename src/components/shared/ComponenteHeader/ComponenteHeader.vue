001a5d5f6efc9233228c099e0053ef796e1cf2b6
Manoela Urias <maanoelaurias@gmail.com>
2024-10-30 22:35:53 -0300

    Estilização do componente

--- src/components/shared/ComponenteHeader/ComponenteHeader.vue
+++ src/components/shared/ComponenteHeader/ComponenteHeader.vue
@@ -1,11 +1,43 @@
 <template>
-
+    <nav>
+        <div class="cabecalho">
+            <h1>sneakers</h1>
+            <ul>
+                <li v-for="(item, index) in ['Collections', 'Men', 'Women', 'About', 'Contact']" :key="index"
+                    @click="selectItem(index)" :class="{ active: selectedItem === index }">
+                    {{ item }}
+                </li>
+            </ul>
+        </div>
+        <div class="usuario-carrinho">
+            <div class="carrinho-usuario">
+                <div class="carrinho">
+                    <img :src="iconeCarrinho" alt="icone-carrinho">
+                </div>
+                <div class="usuario">
+                    <img :src="imagemUsuario" alt="imagem-usuario">
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script setup lang="ts">
-    
+import { ref } from 'vue';
+import iconeCarrinho from '../../../assets/images/icon-cart.svg';
+import imagemUsuario from '../../../assets/images/image-avatar.png';
+
+const selectedItem = ref<number | null>(null);
+
+function selectItem(index: number) {
+    selectedItem.value = index;
+}
 </script>
 
 <style scoped lang="scss">
 @import './ComponenteHeader';
+    .cabecalho ul li.active {
+      border-bottom: solid 4px var(--orange);
+      cursor: pointer;
+    }
 </style>
