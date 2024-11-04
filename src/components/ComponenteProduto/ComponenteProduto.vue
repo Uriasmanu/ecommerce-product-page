fcb66eba82179859e5995d9e02e0339722a38ac2
Manoela Urias <maanoelaurias@gmail.com>
2024-11-04 15:44:01 -0300

    Adição das informações

--- src/components/ComponenteProduto/ComponenteProduto.vue
+++ src/components/ComponenteProduto/ComponenteProduto.vue
@@ -1,23 +1,44 @@
 <template>
     <div class="container-produto">
-        <img :src="fotoProduto1" alt="imagem do produto" class="produto">
-        <ul>
-            <li class="img">
-                <img :src="fotoProduto1Tamb" alt="miniatura do produto">
-            </li>
-            <li class="img">
-                <img :src="fotoProduto2Tamb" alt="miniatura do produto">
-
-            </li>
-            <li class="img">
-                <img :src="fotoProduto3Tamb" alt="miniatura do produto">
-
-            </li>
-            <li class="img">
-                <img :src="fotoProduto4Tamb" alt="miniatura do produto">
-
-            </li>
-        </ul>
+        <div class="imagem-produto">
+            <img :src="fotoProduto1" alt="imagem do produto" class="produto">
+            <ul>
+                <li class="img">
+                    <img :src="fotoProduto1Tamb" alt="miniatura do produto">
+                </li>
+                <li class="img">
+                    <img :src="fotoProduto2Tamb" alt="miniatura do produto">
+                </li>
+                <li class="img">
+                    <img :src="fotoProduto3Tamb" alt="miniatura do produto">
+                </li>
+                <li class="img">
+                    <img :src="fotoProduto4Tamb" alt="miniatura do produto">
+                </li>
+            </ul>
+        </div>
+        <div class="info-produto">
+            <p>Empresa de Tênis</p>
+            <h2>Tênis Edição Limitada Outono</h2>
+            <p>Esses tênis de perfil baixo são o seu companheiro perfeito para um visual casual. Com uma
+                sola de borracha durável, eles resistem a tudo o que o clima pode oferecer.</p>
+            <div class="preco">
+                <p>R$ 125,00</p>
+                <p>50%</p>
+            </div>
+            <p>R$ 250,00</p>
+            <div class="buton-somar">
+                <button>-</button>
+                <input type="text" value="0">
+                <button>+</button>
+            </div>
+            <div class="buton-cart">
+                <button>
+                    <img src="" alt="imagem carrinho">
+                    adicionar ao carrinho
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -31,7 +52,6 @@ import fotoProduto1Tamb from '../../assets/images/image-product-1-thumbnail.jpg'
 import fotoProduto2Tamb from '../../assets/images/image-product-2-thumbnail.jpg'
 import fotoProduto3Tamb from '../../assets/images/image-product-3-thumbnail.jpg'
 import fotoProduto4Tamb from '../../assets/images/image-product-4-thumbnail.jpg'
-
 </script>
 
 <style scoped>
